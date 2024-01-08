4699cef2976ecad51e149a6fe54f4d4dd4c92b47
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-01-08 16:48:11 +0200

    Add: unimplemented xml deserializer

---
+++ src/Mini-Serializer-Deserializer/Deserializers/MiniXmlDeserializer.cs
@@ -0,0 +1,22 @@
+﻿using Mini_Serializer_Deserializer.Serializers.Configurations;
+
+namespace Mini_Serializer_Deserializer.Deserializers
+{
+    public class MiniXmlDeserializer
+    {
+        private readonly MiniXmlSerializerConfigurations _configurations;
+        public MiniXmlDeserializer()
+        {
+            _configurations = new MiniXmlSerializerConfigurations();
+        }
+        public MiniXmlDeserializer(MiniXmlSerializerConfigurations configurations)
+        {
+            _configurations = configurations;
+        }
+
+        public T DeSerialize<T>(string serializeData, T target) where T : new()
+        {
+            return target;
+        }
+    }
+}
